2f9162200961d897adeaca08d1f96220c6462381
Haveelah Mutluri <113948755+havilah02@users.noreply.github.com>
2024-12-09 13:14:23 -0500

    Update dart_00_a_link2.R
    - The addition of a more descriptive box title and help text.
    - Improved handling of edge cases in the findInterval function by adding the all.inside parameter and modifying the pep mutation with pmin.
    - Replacing plotdata function to handle edge cases.

--- modules/max_quant/030_Peptide_Identifications/dart_00_a_link2.R
+++ modules/max_quant/030_Peptide_Identifications/dart_00_a_link2.R
@@ -1,77 +1,86 @@
 init <- function() {
   
   type <- 'plot'
-  box_title <- ''
-  help_text <- ''
+  box_title <- 'Cumulative Peptide Identification Confidence Across Experiments (DART-ID Integration)'
+  help_text <- 'The plot shows the cumulative peptide identification confidence across experiments, integrating DART-ID for improved identification accuracy.'
   source_file <- 'evidence'
-  
-  .validate <- function(data, input) {
-    validate(need(data()[['evidence']], paste0('Upload evidence.txt')))
-  }
-  
-  .plotdata <- function(data, input) {
-    plotdata <- data()[['evidence']][,c('Raw.file', 'PEP','Type')]
-    plotdata <- plotdata %>% dplyr::filter(Type != "MULTI-MATCH")
-    plotdata <- plotdata %>% dplyr::select('Raw.file', 'PEP')
-    # build log10 PEP vector
-    peps <- seq(log10(max(c(min(plotdata$PEP)), 1e-5)), log10(max(plotdata$PEP)), length.out=500)
-    peps <- c(log10(.Machine$double.xmin), peps)
-    
-    plotdata <- plotdata %>%
-      dplyr::mutate(bin=findInterval(PEP, 10**peps)) %>%
-      dplyr::group_by(Raw.file, bin) %>%
-      dplyr::summarise(n=dplyr::n()) %>%
-      dplyr::mutate(cy=cumsum(n),
-                    pep=10**peps[bin])
 
-    return(plotdata)
-  }
-  
-  .plot <- function(data, input) {
-    .validate(data, input)
-    plotdata <- .plotdata(data, input)
-    
-    validate(need((nrow(plotdata) > 1), paste0('No Rows selected')))
-    
-    # Rank the Experiments by most number of peptides observed
-    
-    maxnum <- c()
-    rawnames <- c()
-    
-    for(X in unique(plotdata$Raw.file)){
-      maxnum <- c(maxnum, max(plotdata$cy[plotdata$Raw.file %in% X]) )
-      rawnames <- c(rawnames, X)
+    .validate <- function(data, input) {
+      validate(need(data()[['evidence']], paste0('Upload evidence.txt')))
     }
     
-    names(maxnum) <- rawnames
-    rank_exp <- maxnum[order(maxnum, decreasing = T)]
-    rank_exp_ord <- seq(1, length(rank_exp),1)
-    names(rank_exp_ord) <- names(rank_exp)
-    plotdata$rank_ord <- NA
-    
-    for(X in levels(plotdata$Raw.file)) {
-      plotdata$rank_ord[plotdata$Raw.file %in% X] <- rank_exp_ord[X]
+    .plotdata <- function(data, input) {
+      plotdata <- data()[['evidence']][,c('Raw.file', 'PEP','Type')]
+      plotdata <- plotdata %>% dplyr::filter(Type != "MULTI-MATCH")
+      plotdata <- plotdata %>% dplyr::select('Raw.file', 'PEP')
+      
+      # build log10 PEP vector
+      peps <- seq(log10(max(c(min(plotdata$PEP)), 1e-5)), log10(max(plotdata$PEP)), length.out=500)
+      peps <- c(log10(.Machine$double.xmin), peps)
+      #adjust the bins to match 'peps' length
+      # better for handling edge cases
+      plotdata <- plotdata %>%
+        dplyr::mutate(bin=findInterval(PEP, 10**peps, all.inside = TRUE)) %>%
+        dplyr::group_by(Raw.file, bin) %>%
+        dplyr::summarise(n=dplyr::n()) %>%
+        dplyr::mutate(cy=cumsum(n),
+                      pep=10**peps[pmin(bin, length(peps))]) # pmin finds the minimum value for each subscript from a set of vectors
+     
+       # plotdata <- plotdata %>%
+       #  dplyr::mutate(bin=findInterval(PEP, 10**peps)) %>%
+       #  dplyr::group_by(Raw.file, bin) %>%
+       #  dplyr::summarise(n=dplyr::n()) %>%
+       #  dplyr::mutate(cy=cumsum(n),
+       #                pep=10**peps[bin])
+      
+      return(plotdata)
+    }
+  
+    .plot <- function(data, input) {
+      .validate(data, input)
+      plotdata <- .plotdata(data, input)
+      
+      validate(need((nrow(plotdata) > 1), paste0('No Rows selected')))
+      
+      # Rank the Experiments by most number of peptides observed
+      
+      maxnum <- c()
+      rawnames <- c()
+      
+      for(X in unique(plotdata$Raw.file)){
+        maxnum <- c(maxnum, max(plotdata$cy[plotdata$Raw.file %in% X]) )
+        rawnames <- c(rawnames, X)
+      }
+      
+      names(maxnum) <- rawnames
+      rank_exp <- maxnum[order(maxnum, decreasing = T)]
+      rank_exp_ord <- seq(1, length(rank_exp),1)
+      names(rank_exp_ord) <- names(rank_exp)
+      plotdata$rank_ord <- NA
+      
+      for(X in levels(plotdata$Raw.file)) {
+        plotdata$rank_ord[plotdata$Raw.file %in% X] <- rank_exp_ord[X]
+      }
+      
+      cc <- scales::seq_gradient_pal('red', 'blue', 'Lab')(seq(0, 1, length.out=length(rank_exp_ord)))
+      
+      ggplot() + 
+        annotate("text", x = 4, y = 25, size=8, label = "The follow plots implement DART-ID\nto update identification confidence:\n\ndart-id.slavovlab.net") + 
+        theme_void()
+      
     }
     
-    cc <- scales::seq_gradient_pal('red', 'blue', 'Lab')(seq(0, 1, length.out=length(rank_exp_ord)))
-    
-    ggplot() + 
-      annotate("text", x = 4, y = 25, size=8, label = "The follow plots implement DART-ID\nto update identification confidence:\n\ndart-id.slavovlab.net") + 
-      theme_void()
-    
-  }
-  
-  return(list(
-    type=type,
-    box_title=box_title,
-    help_text=help_text,
-    source_file=source_file,
-    validate_func=.validate,
-    plotdata_func=.plotdata,
-    plot_func=.plot,
-    box_width=12, # bootstrap column units
-    plot_height=500, # pixels
-    report_plot_width=7, # inches
-    report_plot_height=5 # inches
-  ))
+    return(list(
+      type=type,
+      box_title=box_title,
+      help_text=help_text,
+      source_file=source_file,
+      validate_func=.validate,
+      plotdata_func=.plotdata,
+      plot_func=.plot,
+      box_width=12, # bootstrap column units
+      plot_height=500, # pixels
+      report_plot_width=7, # inches
+      report_plot_height=5 # inches
+    ))
 }
